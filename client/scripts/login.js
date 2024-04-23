e72ee4ae0c4ae2287a4ebe75719e4090a98164a1
Doris <dodo.diallo@gmail.com>
2024-04-23 16:53:10 +0200

    Front login linked to back w/ fetch and form

---
+++ client/scripts/login.js
@@ -0,0 +1,27 @@
+async function loginFront() {
+    
+    const form = {
+        email: document.querySelector("#email"),
+        password: document.querySelector("#pwd"),
+    };
+    
+    
+    // TODO : éventuellement try/catch pour gestion d'erreur du fetch en lui-même
+    const response = await fetch("/login", {
+        method: "POST",
+        body:   JSON.stringify({
+            email: form.email.value,
+            password: form.password.value,
+        })
+    });
+    // Vérification
+    if (response.ok){
+        const data = await response.json();
+        alert("ok");
+    }
+    else{
+        alert("Mauvais identifiant ou mot de passe");
+        //TODO : éventuellement un affichage dynamique sur la page ?
+    }
+
+}
