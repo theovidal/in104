e7a204ab596933a88f410c3524e0c91011df19f1
Your Name <seydoulaara.sene@gmail.com>
2024-04-30 15:38:40 +0200

    Brought back reverted commit : 'modifs sur routes de code'

--- server/core/codes.js
+++ server/core/codes.js
@@ -1,14 +1,15 @@
 const mutex = new Mutex();
 let codes = {};
-//fonction qui insert un code dedans, avec le mutex, dans le release il va juste (enft g pas compris ça me fume)
-//Bcp de choses pas claires dans ma tête
 
-function insert_code(req, res) {
+
+function insert_code(key, value) {
     mutex
         .acquire()
         .then(function (release) {
             //codes à l'id id_professeur = code, jsp comment le traduire en code...
-
+            codes[key] = value;
             release();
         });
 }
+
+module.exports = {insert_code}
