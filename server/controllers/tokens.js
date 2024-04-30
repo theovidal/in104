ccd4d5c85f7c9880d433697f04163c3fa32a7bf7
Sh3xe <sh3xe@laposte.net>
2024-04-30 12:34:34 +0200

    ajout des fonctions de la bdd relatives aux tokens

--- server/controllers/tokens.js
+++ server/controllers/tokens.js
@@ -1,3 +1,6 @@
+const db = require("../../db/models");
+const {Op} = require("sequelize")
+
 /**
  * créer un jeton d'identification pour userId qui expire le expireAt
  * 
@@ -6,17 +9,50 @@
  * @param {*} expireAt une date sous format standard ISO, par exemple: (new Date()).toISOString()
  */
 exports.create = (userId, token, expireAt) => {
-
+	return new Promise((resolve, reject) => {
+		db.Tokens.create({userId, token, expireAt})
+			.then( token => resolve(token.dataValues))
+			.catch( err => reject(err) );
+	});
 }
 
 /**
- * Vérifie que [token] est bien associé à [userId] et le retire s'il est expiré
- * @param {*} userId 
- * @param {*} token 
- * @returns un booléen
+ * Vérifie que [token] est bien associé à [userId] et n'a pas expiré
+ * @param {int} userId 
+ * @param {string} token 
+ * @returns {Promise<bool>} un booléen
  */
 exports.test = (userId, token) => {
+	return new Promise((resolve, reject) => {
+		db.Tokens.findAll({
+			where: {
+				userId,
+				token,
+				expireAt: { [Op.gte]: (new Date()) }
+			}
+		}).then( values => {
+			const dataValues = values.map( val => val.dataValues);
+			resolve( dataValues.length != 0 )
+		}).catch( err => reject(err) )
+	});
+}
 
+/**
+ * supprimer tout les jetons de [userId] qui ont expirés
+ * @param {int} userId 
+ * @returns {Promise<>}
+ */
+exports.removeExpiredTokens = (userId) => {
+	return new Promise( (resolve, reject) => {
+		db.Tokens.destroy({
+			where: {
+				userId,
+				expireAt: { [Op.lt]: (new Date()) }
+			}
+		}).then( _ => {
+			resolve()
+		}).catch( err => reject(err) )
+	})
 }
 
 /**
@@ -25,5 +61,14 @@ exports.test = (userId, token) => {
  * @param {*} token 
  */
 exports.remove = (userId, token) => {
-
+	return new Promise( (resolve, reject) => {
+		db.Tokens.destroy({
+			where: {
+				userId,
+				token
+			}
+		}).then( _ => {
+			resolve()
+		}).catch( err => reject(err) )
+	})
 }
