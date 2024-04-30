4629205cbf956a665dcabe5529ee2f9a480b779b
Sh3xe <sh3xe@laposte.net>
2024-04-30 14:56:34 +0200

    mise à jour des tests

--- server/tests/db.test.js
+++ server/tests/db.test.js
@@ -3,57 +3,56 @@ const users = require("../controllers/users");
 const tokens = require("../controllers/tokens");
 
 test("token db controller", async() => {
-	db.sync({force: true}).then(async() => {
-		try {
-			const user = await users.create({
-				firstname: "arnaud",
-				lastname: "pelissier",
-				email: "a@a.a",
-				role: "eleve",
-				password: "1234"
-			});
+	try {
+		await db.sync({force: true})
+		const user = await users.create({
+			firstname: "arnaud",
+			lastname: "pelissier",
+			email: "a@a.a",
+			role: "eleve",
+			password: "1234"
+		});
 
-			let expireAt = new Date();
-			expireAt.setDate(expireAt.getDate()+1);
+		let expireAt = new Date();
+		expireAt.setDate(expireAt.getDate()+1);
 
-			await tokens.create(user.id, "abcde1992", expireAt);
+		await tokens.create(user.id, "abcde1992", expireAt);
 
-			expect( await tokens.test(1, "abcde1992") ).toBe(true);
-			expect( await tokens.test(1, "abddcde1992") ).toBe(false);
-		}
-		catch(err) {
-			expect(true).toBe(false);
-		}
-	}).catch( () => {
+		expect( await tokens.test(1, "abcde1992") ).toBe(true);
+		expect( await tokens.test(1, "abddcde1992") ).toBe(false);
+	}
+	catch(err) {
+		console.error(err);
 		expect(true).toBe(false);
-	})
+	}
 })
 
 test("user db controller", async() => {
-	db.sync({force: true}).then(async() => {
-		try {
-			const user = await users.create({
-				firstname: "arnaud",
-				lastname: "pelissier",
-				email: "a@a.a",
-				role: "eleve",
-				password: "1234"
-			});
+	try {
+		await db.sync({force: true})
 
-			expect( (await users.getByEmail("a@a.a")).id ).toBe(user.id)
+		const user = await users.create({
+			firstname: "arnaud",
+			lastname: "pelissier",
+			email: "a@a.a",
+			role: "eleve",
+			password: "1234"
+		});
 
-			await users.updateById(user.id, {email: "aa@aa.aa"})
+		expect( (await users.getByEmail("a@a.a")).id ).toBe(user.id)
 
-			expect( (await users.getByEmail("aa@aa.aa")).id ).toBe(user.id)
+		await users.updateById(user.id, {email: "aa@aa.aa"})
 
-			await users.removeByEmail("aa@aa.aa")
+		expect( (await users.getByEmail("aa@aa.aa")).id ).toBe(user.id)
 
-			expect( (await users.getByEmail("aa@aa.aa")).id ).toBe(undefined)
-		}
-		catch(err) {
-			expect(true).toBe(false);
-		}
-	}).catch( () => {
-		expect(true).toBe(false);
-	})
+		expect( await users.testCredentialsById(user.id, "1234") ).toBe(true)
+		expect( await users.testCredentialsById(user.id, "12345") ).toBe(false)
+
+		await users.removeByEmail("aa@aa.aa")
+
+		expect( (await users.getByEmail("aa@aa.aa")) ).toBe(null)
+	} catch(err) {
+		console.error(err)
+		expect(true).toBe(false)
+	}
 })
