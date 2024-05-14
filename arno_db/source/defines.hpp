ff26c3a254b9dd76d6e942503c9886756fe01590
Sh3xe <sh3xe@laposte.net>
2024-05-14 14:56:04 +0200

    ajout des conditions

--- arno_db/source/defines.hpp
+++ arno_db/source/defines.hpp
@@ -9,7 +9,28 @@ enum class DT
 {
 	NIL,
 	INT,
-	TEXT
+	TEXT,
+	FLOAT,
+	BOOL
 };
+
+std::string to_string( DT type )
+{
+	switch( type )
+	{
+		case DT::NIL:
+			return "null";
+		case DT::FLOAT:
+			return "real";
+		case DT::INT:
+			return "integer";
+		case DT::TEXT:
+			return "text";
+		case DT::BOOL:
+			return "bool";
+		default:
+			return "unknown";
+	}
+}
 
 }
