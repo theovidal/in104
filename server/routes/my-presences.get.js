84470cd2b372da5f9bb6f0b957bd12066b79b715
Your Name <seydoulaara.sene@gmail.com>
2024-05-14 17:42:02 +0200

    clarifié ce que fait my-presence

--- server/routes/my-presences.get.js
+++ server/routes/my-presences.get.js
@@ -1,4 +1,4 @@
-// Return all the presences of pupils, so they can see at which lectures they were marked as absent
+// Return all of a pupils' absences, as lectures
 module.exports = function myPresencesRoute(req, res) {
 
 }
