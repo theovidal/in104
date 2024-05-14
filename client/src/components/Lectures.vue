a2a6caf221e1fe5f6cd11e8e2a2f69bff51af013
Théo VIDAL <theo.vidal@ensta-paris.fr>
2024-05-14 16:44:43 +0200

    wip: add different home views for users : - For teachers: the lectures they have - For pupils : their absences - For the administration : a full report of all the presences and absences

---
+++ client/src/components/Lectures.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="lectures_wrap">
+    <h1>Mes cours</h1>
+    <RouterLink
+      v-for="lecture in lectures"
+      :key="lecture.id"
+      class="lecture_block"
+      :to="`/generate/${lecture.id}`"/>
+    <div class="lecture_block_content">
+      <h2>{{ course.name }} - {{ lecture.date }}</h2>
+      <p>Durée : {{ lecture.duration }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { endpoints, request } from '@/utils/api.js'
+import { getLectureCourses } from '@/utils/lectures.js'
+
+const response = await request(endpoints.lectures)
+const lectures = await response.json()
+
+const courses = getLectureCourses(lectures);
+</script>
---
+++ client/src/components/PresenceReport.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="presence_report_wrap">
+    <table>
+      <thead>
+        <tr>
+          <th scope="col">Nom</th>
+          <th scope="col">Prénom</th>
+          <th scope="col">Cours</th>
+          <th scope="col">Date</th>
+          <th scope="col">Présent ?</th>
+        </tr>
+      </thead>
+      <tbody>
+      <tr
+        v-for="presence in presences"
+        :key="`${presence.userId} ${presence.lectureId}`">
+        <th>{{ presence.user.lastName }}</th>
+        <th>{{ presence.user.firstName }}</th>
+        <th>{{ presence.course.name }}</th>
+        <th>{{ presence.lecture.date }}</th>
+        <th>{{ presence.isPresent ? 'Oui' : 'Non'}}</th>
+      </tr>
+      </tbody>
+    </table>
+    <button @click="exportPresences">Exporter le rapport</button>
+  </div>
+</template>
+
+<script>
+import { request } from '@/utils/api.js';
+import { saveAs } from 'file-saver';
+
+const response = await request(endpoints.presenceReport);
+const presences = await response.json();
+
+function exportPresences() {
+  let data = "Nom,Prénom,Cours,Date,Présent ?\n";
+  for (const presence of presences) {
+    data += `${presence.user.firstName},${presence.user.lastName},${presence.course.name},${presence.lecture.date},${presence.isPresent ? 'Oui' : 'Non'}\n`;
+  }
+
+  const blob = new Blob([data], {
+    type: 'text/csv'
+  });
+  saveAs(blob, "presences.csv");
+}
+</script>
