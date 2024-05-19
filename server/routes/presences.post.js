e8c6f9b320ae776438757453cebc0a5c24b63554
Your Name <seydoulaara.sene@gmail.com>
2024-05-19 18:53:54 +0200

    progres dans presences

--- server/routes/presences.post.js
+++ server/routes/presences.post.js
@@ -6,35 +6,35 @@ const users_fun = require('../controllers/users');
 const courses_fun = require('../controllers/courses');
 
 
-module.exports = function createPresence(req, res) {
+module.exports = async function createPresence(req, res) {
     if (res.locals.user.role === 'eleve') {
 
         //Verifications
 
         qrcode = req.query.code;
-        const lecture_id = read_lecture(qr_code);
+        const lecture_id = read_lecture(qrcode);
 
         if (lecture_id === undefined) {
-            res.status(400).json({
+            return res.status(400).json({
                 error: 'Séance non précisée'
             })
         }
 
-        const lecture = lectures_fun.getById(lecture_id);
+        const lecture = await lectures_fun.getById(lecture_id);
         if (lecture === null) {
-            res.status(400).json({
+            return res.status(400).json({
                 error: 'Séance inexistante'
             })
         }
 
-        const user = users_fun.getByEmail(res.locals.users.email);
+        const user = await users_fun.getByEmail(res.locals.users.email);
         if (user === null) {
-            res.status(400).json({
+            return res.status(400).json({
                 error: 'Utilisateur inexistant'
             })
         }
 
-        const course = courses_fun.getById(lecture.courseId);
+        const course = await courses_fun.getById(lecture.courseId);
 
 
         const date_requete = new Date();
@@ -43,15 +43,16 @@ module.exports = function createPresence(req, res) {
         date_fin.setTime(date_fin.getTime() + 3_600_000);
 
         if (date_debut > date_requete || date_fin < date_requete) {
-            res.status(400).json({
+            return res.status(400).json({
                 error: 'Horaire incompatible'
             })
         }
         //ce sera beginDate et endDate dès que Arnaud l'aura fait
 
-        const liste_eleves = courses_fun.getAttendantsById(course.id);
+        const liste_eleves = await courses_fun.getAttendantsById(course.id);
+        console.log(liste_eleves);
         if (liste_eleves(user) === undefined) {
-            res.status(400).json({
+            return res.status(400).json({
                 error: 'Eleve et cours incompatibles'
             })
         }
@@ -59,19 +60,11 @@ module.exports = function createPresence(req, res) {
 
         //On met l'élève présent
 
-        lectures_fun.updatePresence(lecture_id, user.id, true);
+        await lectures_fun.updatePresence(lecture_id, user.id, true);
 
-    } else {
-<<<<<<< HEAD
-    res.status(403).json({
+    } else { 
+    return res.status(403).json({
         error: 'Erreur dans la génération du code'
     })
     }
-
-=======
-        res.status(403).json({
-            error: 'Erreur dans la génération du code'
-        })
-    }
->>>>>>> 86ab7369644970fb0a86f5c872d6c3a74d61b15c
 }
