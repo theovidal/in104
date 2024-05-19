d3ee2f64c58b1ba646d298f6e6161d2bac234db7
Théo VIDAL <theo.vidal@ensta-paris.fr>
2024-05-19 18:37:46 +0200

    refactor: convert lecture codes to jwt, which are more secure and can store information inside

--- server/routes/codes.post.js
+++ server/routes/codes.post.js
@@ -1,29 +1,9 @@
-//crée le code sous-jacent au qr code généré lorsqu'un professeur (et non pas un élève) se connecte
-
-const { Courses } = require('../../db/models')
-const { insert_lecture, lectures } = require('../core/lectures')
 const courses_fun = require('../controllers/courses');
 const lectures_fun = require('../controllers/lectures');
 const jwt = require('jsonwebtoken')
 
+//crée le code sous-jacent au qr code généré lorsqu'un professeur (et non pas un élève) se connecte
 module.exports = async function createCode(req, res) {
-
-    //fonction de génération aléatoire de chaîne de caractère, pas optimal mais je le laisse en argument pour la soutenance
-
-    /*function makecode(length) {
-        let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const charactersLength = characters.length;
-        let counter = 0;
-        while (counter < length) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-            counter += 1;
-        }
-        return result;
-    }*/
-
-    //génération et envoie du code, si l'utilisateur est un professeur
-
     if (res.locals.user.role !== 'professeur') return res.status(403).json({
         error: 'Acces interdit'
     })
@@ -64,7 +44,8 @@ module.exports = async function createCode(req, res) {
     }
 
     const code = jwt.sign({
-        lectureId: lecture.id,
+        id: lecture.id,
+        ppl: attendants.map(user => user.id)
     }, process.env.CODES_SECRET, { expiresIn: `${process.env.CODES_EXPIRATION}s` });
 
     res.json({
