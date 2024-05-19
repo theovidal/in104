6d1f827f1a19a6e418fa627fd2d954e9d37e0616
Your Name <seydoulaara.sene@gmail.com>
2024-05-19 20:43:51 +0200

    Waiting for controller capable of giving me all lectures, for presence.get.js

--- server/routes/presences.get.js
+++ server/routes/presences.get.js
@@ -1,8 +1,8 @@
 //Donne les présences/absences d'un ou des élève(s), selon le cas d'utilisation
 //query - presence : true/false/undefined, user_id : user_id/undefined, lecture_id : lecture_id/undefined
+const courses = require("../controllers/courses");
 
-module.exports = function getPresences(req, res) {
 
-}
-  res.send();
+module.exports = async function getPresences(req, res) {
+    
 }
