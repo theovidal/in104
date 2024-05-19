7027ca6377ec2f38042aa3a75a28a2aea8290634
Your Name <seydoulaara.sene@gmail.com>
2024-05-19 19:51:36 +0200

    Deleted lectures.js qui gérait le mutex des lectures, devenu obsolète

--- server/routes/presences.get.js
+++ server/routes/presences.get.js
@@ -1,3 +1,8 @@
+//Donne les présences/absences d'un ou des élève(s), selon le cas d'utilisation
+//query - presence : true/false/undefined, user_id : user_id/undefined, lecture_id : lecture_id/undefined
+
 module.exports = function getPresences(req, res) {
+
+}
   res.send();
 }
