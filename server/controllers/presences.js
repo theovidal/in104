6b0c9110670a3b843ecbc2b2d96f192fb2e4a43e
Théo VIDAL <theo.vidal@ensta-paris.fr>
2024-05-20 15:07:58 +0200

    feat: add presences controller

--- server/controllers/presences.js
+++ server/controllers/presences.js
@@ -1,8 +1,21 @@
+const db = require('../../db/models')
 
-exports.getUserPresences = (userId) => {
+exports.getPresences = (lectureId = undefined, userId = undefined, isPresent = undefined, includeUser = false, includeLecture = false) => {
+  let where = {
+    userId,
+    lectureId,
+    isPresent
+  }
+  for (const i in where) {
+    if (where[i] === undefined) delete where[i];
+  }
 
-}
-
-exports.getAllPresences = () => {
-	
+  return new Promise((resolve, reject) => {
+    db.Presences.findAll({
+      where
+    }).then( vals => {
+      let dataValues = vals.map(value => value.dataValues)
+      resolve(dataValues);
+    }).catch( reject );
+  });
 }
